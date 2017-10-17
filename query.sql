46dd820daf301a19b5cb90348d8234833b61ee44
Tanner Burge-Beckley <tburgeb@siue.edu>
2017-10-16 21:00:58 -0500

    Updated query to describe tables

--- query.sql
+++ query.sql
@@ -1,5 +1,18 @@
 USE phlebotomy;
 
+DESCRIBE address;
+DESCRIBE billedTo;
+DESCRIBE consults;
+DESCRIBE doctor;;
+DESCRIBE insuranceCompany;
+DESCRIBE invoice;
+DESCRIBE livesAt;
+DESCRIBE patient;
+DESCRIBE performs;
+DESCRIBE phlebotomist;
+DESCRIBE specialist;
+DESCRIBE visit;
+
 SELECT * FROM address;
 SELECT * FROM billedTo;
 SELECT * FROM consults;
