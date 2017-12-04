e0e1ff69659c3226f96c0f0d8e57cb63c94b59f7
Tanner Burge-Beckley <tburgeb@siue.edu>
2017-12-03 22:26:42 -0600

    Fixed the thing

--- dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php  
-    define('DB_USER', 'tburgeb');
-    define('DB_PWD', 'mMdtBB81409');
-    define('DB', 'phlebotomyBig');
-    define('HOST', 'localhost');
-    define('DB_CONNECTION_STRING', 'mysql:host='. HOST.';dbname='.DB);
-?>
