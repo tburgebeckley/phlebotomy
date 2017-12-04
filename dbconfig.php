229c916568e4d33c4fa8f8721f5f816255616575
Tanner Burge-Beckley <tburgeb@siue.edu>
2017-12-03 21:47:28 -0600

    Form structure for visit.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,2 +1,10 @@
 <?php
+       $host = 'localhost';
+       $dbname = 'phlebotomyBig';
+       $username = 'tburgeb';
+       $password = 'mMdtBB81409';
+
+       define('DB_CONNECTION_STRING' ,"mysql:host=$host;dbname=$dbname");
+       define('DB_USER', "$username");
+       define('DB_PWD', "$password");
 ?>
--- p6/phlebotomyBrowser/visit.php
+++ p6/phlebotomyBrowser/visit.php
@@ -2,31 +2,49 @@
     require_once('function.php');
     if ($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        $form = file_get_contents('../visit.html');
+        $form = file_get_contents('../html/visit.html');
 
         $patients = ['Adrian', 'Lala'];
         pageHeader('Log New Visit', 'visit');
-
+        $phlebs = phlebs();
+        $patients = patients();
+        $page = format($form, array('path'=>$_SERVER['PHP_SELF'], 'patients'=>$patients, 'phlebotomists'=>$phlebs));
+        echo ($page);
         pageFooter();
+    } else if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+    {
+        var_dump($_POST);
     }
-
-
-
-
-
-
-
 
     function patients()
     {
         $pdo = pdo_construct();
-        $query = 'SELECT `name` FROM patient';
+        $query = 'SELECT `name` FROM patient ORDER BY name';
         $options = "";
 
         $result = pdo_query($pdo,$query);
-
-        foreach ($result->fetch() as $row)
+        
+        while ($row = $result->fetch(PDO::FETCH_ASSOC))
         {
-            $temp = "\t\t\t";
+            $temp = "\t\t\t\t\t<option value='{}'>{}</option>\n";
+            $options = $options . format($temp, array($row['name'], $row['name']));
         }
+
+        return $options;
+    }
+
+    function phlebs() {
+        $pdo = pdo_construct();
+        $query = 'SELECT `name` FROM phlebotomist';
+        $options = "";
+
+        $result = pdo_query($pdo,$query);
+        
+        while ($row = $result->fetch(PDO::FETCH_ASSOC))
+        {
+            $temp = "\t\t\t\t\t<option value='{}'>{}</option>\n";
+            $options = $options . format($temp, array($row['name'], $row['name']));
+        }
+
+        return $options;
     }
